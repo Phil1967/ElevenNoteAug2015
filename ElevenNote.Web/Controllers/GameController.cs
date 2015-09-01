2f80b405b935ccfb35cb968d1f183eda13c34770
Phillip Midkiff <phillip.midkiff@gmail.com>
2015-09-01 15:40:05 -0400

    Easter egg

---
+++ ElevenNote.Web/Controllers/GameController.cs
@@ -0,0 +1,43 @@
+﻿using ElevenNote.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.Web.Controllers
+{
+    public class GameController : Controller
+    {
+        #region GET
+        // GET: Game
+        [HttpGet]
+        [ActionName("Index")]
+        public ActionResult IndexGet()
+        {
+            var correctAnswer = new Random().Next(1, 10);
+            Session["Answer"] = correctAnswer;
+            return View();
+        }
+        #endregion
+        [HttpPost]
+        [ActionName("Index")]
+        public ActionResult IndexPost(GuessingGameViewModel model)
+        { 
+            if (!ModelState.IsValid)
+            {
+                //if there was an error.
+            }
+            else
+            {
+                var numberToCompareTo = (int)Session["Answer"];
+                var isCorrect = model.Guess == numberToCompareTo;
+                //guessing logic
+                ViewBag.GuessWasCorrect = isCorrect;
+
+            }
+            return View(model);
+        }
+    }
+
+}
---
+++ ElevenNote.Web/Models/GuessingGameViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ElevenNote.Web.Models
+{
+    public class GuessingGameViewModel
+    {
+        public String Name { get; set; }
+
+        public int Guess { get; set; }
+        
+    }
+}
